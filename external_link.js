e7273be0917f1bf73fa772d66ad510bd957eb0eb
Josh N <nerocom@gmail.com>
2021-02-05 16:31:34 -0700

    Initial Commit

---
+++ external_link.js
@@ -0,0 +1,12 @@
+document.body.style.border = "5px solid red";
+
+$('a').each(function() {
+    let href = $(this).attr('href');
+    if (href != null) {
+      if (href.slice(0,1) != '/' && href.slice(0,1) != '#' && href.slice(0,10) != 'javascript') {
+        if (href.indexOf('atlassian.net') === -1) {
+          $(this).attr('target','_blank');
+        }
+      }
+    }
+  });
